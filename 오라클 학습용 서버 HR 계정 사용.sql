3ae0f68d79aa5e18931141aa0b53dc9e833a83bd
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-03-12 12:11:43 +0900

    210312

---
+++ 오라클 학습용 서버 HR 계정 사용.sql
@@ -0,0 +1,16 @@
+SELECT employee_id, first_name, last_name, salary, job_id
+FROM employees;
+
+SELECT * FROM employees;
+
+SELECT first_name, last_name, salary, salary+100
+FROM employees;
+
+INSERT INTO employees VALUES(207, 'JOHN', 'AUSTIN', 'AUSTINMAIL', 
+'515.123.9999', '02/06/07', 'SH_CLERK', NULL, NULL, 124, 50);
+
+SELECT * FROM employees;
+
+SELECT employee_id, first_name, salary, salary+300 FROM employees;
+
+SELECT employee_id, first_name, salary, NVL(salary, 0)+300 FROM employees;
--- 오라클 학습용 서버 scott 계정 사용.sql
+++ 오라클 학습용 서버 scott 계정 사용.sql
@@ -1,1 +1,24 @@
-select * from emp;
+SELECT empno, ename, sal, comm, comm+300, NVL(comm,0) FROM emp;
+
+SELECT ename AS name, sal, sal*12 "Annual Salary" FROM emp;
+
+SELECT ename AS name, sal, sal*12 as "Annual Salary" FROM emp;
+
+SELECT ename 결과1, 'Abc' 결과2, sal 결과3, 500 결과4 FROM emp;
+
+SELECT ename||':'||empno||':'||job FROM emp;
+
+SELECT ename||' is a '||job AS "Employees Details" FROM emp;
+
+SELECT job FROM emp;
+
+SELECT ALL job FROM emp;
+
+SELECT DISTINCT job FROM emp;
+
+SELECT DISTINCT deptno, job FROM emp;
+
+SELECT ROWID, ROWNUM, ename, sal FROM emp;
+
+
+
