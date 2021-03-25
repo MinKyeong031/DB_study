376febec9f7741b82df01fc7d8462345671c8632
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-03-25 10:19:00 +0900

    210325

--- 오라클 학습용 서버 HR 계정 사용.sql
+++ 오라클 학습용 서버 HR 계정 사용.sql
@@ -17,3 +17,54 @@ SELECT employee_id, first_name, salary, NVL(salary, 0)+300 FROM employees;
 
 --SELECT emplyee_id 사원번호, salary FROM employees WHERE 사원번호 > 200;
 SELECT employee_id 사원번호, salary FROM employees WHERE employee_id > 200;
+
+SELECT first_name, SUBSTR(first_name, 1, 3), SUBSTR(first_name,3), SUBSTR(first_name, -3, 2) FROM employees WHERE department_id = 10; 
+
+SELECT SUBSTR('ABCDEFG', 3),
+SUBSTR('ABCDEFG', 6),
+SUBSTR('ABCDEFG', -4),
+SUBSTR('ABCDEFG', -1)
+FROM dual;
+
+SELECT SUBSTRB('ABCDEFG', 6),
+SUBSTRB('한글입니다', 4),
+SUBSTRB('한글입니다', 5),
+SUBSTRB('한글입니다', 6)
+FROM dual;
+
+SELECT parameter, value
+FROM nls_database_parameters
+WHERE parameter = 'NLS_CHARACTERSET';
+
+SELECT SUBSTRB('I am here with you', 5, 3) 결과1,
+SUBSTR ('나 여기 있어', 5, 3) 결과2,
+SUBSTRB ('나 여기 있어', 5, 3) 결과3
+FROM dual;
+
+SELECT * FROM dual;
+
+SELECT SYSDATE FROM dual;
+
+SELECT SYSTIMESTAMP FROM dual;
+
+SELECT 3*4 FROM dual;
+
+SELECT first_name, LPAD(first_name, 15, '*'), salary, LPAD(salary, 10, '*') FROM employees WHERE department_id = 20;
+
+SELECT first_name, RPAD(first_name, 15, '*'), salary, RPAD(salary, 10, '*') FROM employees WHERE department_id = 20;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 오라클 학습용 서버 scott 계정 사용.sql
+++ 오라클 학습용 서버 scott 계정 사용.sql
@@ -66,3 +66,51 @@ SELECT DISTINCT job, sal+comm FROM emp ORDER BY sal;
 
 SELECT DISTINCT deptno, job FROM emp ORDER BY 2;
 
+SELECT empno, ename, job, LOWER(job), INITCAP(job) FROM emp WHERE deptno = 10;
+
+SELECT INSTR('ABCDEFG', 'A'),
+INSTR('ABCDEFG','E'),
+INSTR('ABCDEFG','CDE'),
+INSTR('ABCDEFG','DBC')
+FROM dual;;
+
+SELECT INSTR('ABBCCCDDD', 'C', 5, 2) IN1, 
+INSTR('ABBCCCDDD','C',6, 2) IN2,
+INSTR('ABBCCCDDD','D',8, 2) IN3,
+INSTR('ABBCCCDDD','D',3, 2) IN4,
+INSTR('ABBCCCDDD','A',4, 1) IN5,
+INSTR('ABBCCCDDD','C',1, 3) IN6
+FROM dual;
+
+SELECT INSTRB('ABCDEFG', 'B'),
+INSTRB('가나라다라마', '나'),
+INSTRB('가나다라마', '다')
+FROM dual;
+
+SELECT parameter, value
+FROM nls_database_parameters
+WHERE parameter = 'NLS_CHARACTERSET';
+
+SELECT ename, job, LTRIM(job, 'A'), sal, LTRIM(sal, 2) FROM emp WHERE deptno = 20;
+
+SELECT ename, job, RTRIM(job, 'T'), sal, RTRIM(sal, 0) FROM emp WHERE deptno = 10;
+
+SELECT ename, REPLACE(ename, 'AR', '*?') 변경결과 FROM emp;
+
+SELECT ename, TRANSLATE(ename, 'AR', '*?') 변경결과 FROM emp;
+
+SELECT TRIM(LEADING 'A' FROM 'AABDCADD') 걸과1,
+ TRIM('A' FROM 'AABDCADD') 걸과2,
+ TRIM(TRAILING 'D' FROM 'AABDCADD') 걸과3
+ FROM dual;
+ 
+
+
+
+
+
+
+
+
+
+
